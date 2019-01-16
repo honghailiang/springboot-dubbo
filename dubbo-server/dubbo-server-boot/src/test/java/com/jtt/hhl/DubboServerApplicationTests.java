3b259d2de780cb7b584437245e86967b698eb39b
honghailiang <honghailaing666@126.com>
2019-01-16 11:22:32 +0800

    add test

--- dubbo-server/dubbo-server-boot/src/test/java/com/jtt/hhl/DubboServerApplicationTests.java
+++ dubbo-server/dubbo-server-boot/src/test/java/com/jtt/hhl/DubboServerApplicationTests.java
@@ -1,7 +1,10 @@
 package com.jtt.hhl;
 
+import com.jtt.hhl.service.TestDubboService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,9 +12,18 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DubboServerApplicationTests {
 
+    @Autowired
+    private TestDubboService testDubboService;
+
     @Test
     public void contextLoads() {
     }
 
+
+    @Test
+    public void testDubboService() {
+        String string = testDubboService.testDubboService("honghailiang");
+        Assert.assertEquals("honghailiang", string);
+    }
 }
 
